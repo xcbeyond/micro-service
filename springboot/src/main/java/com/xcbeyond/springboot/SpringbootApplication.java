c95400119c9a0f702b93c81691533718c87806f0
xuchao <xuchao@wondersoft.cn>
2018-07-02 17:49:47 +0800

    构建SpringBoot工程

---
+++ springboot/src/main/java/com/xcbeyond/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.xcbeyond.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * SpringBoot启动类
+ * @author xcbeyond
+ * 2018年7月2日下午5:41:45
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
