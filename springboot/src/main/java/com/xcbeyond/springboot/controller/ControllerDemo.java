5c698deaa423be58c990d90f9ba34e76212fc8e8
xcbeyond <xcbeyond157@163.com>
2018-07-12 11:29:03 +0800

    添加注释

--- springboot/src/main/java/com/xcbeyond/springboot/controller/ControllerDemo.java
+++ springboot/src/main/java/com/xcbeyond/springboot/controller/ControllerDemo.java
@@ -12,7 +12,10 @@ import com.xcbeyond.springboot.config.RandomConfig;
  * 2018年7月5日下午11:22:49
  */
 
+//是@Controller和@ResponseBody的合集,表示这是个控制器bean,并且是将函数的返回值直接填入HTTP响应体中,是REST风格的控制器。
 @RestController
+//提供路由信息，负责URL到Controller中的具体函数的映射
+@RequestMapping("/demo")
 public class ControllerDemo {
 	@Resource
 	private RandomConfig randomConfig;
