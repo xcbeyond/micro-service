1ceac7e609469b980a6d57254f1512ad575007d6
xcbeyond <xcbeyond157@163.com>
2018-07-13 18:21:28 +0800

    1、pom.xml中添加依赖包mysql-connector-java 2、修改UserDaoJdbcTemplateImplTest测试类

--- springboot/src/test/java/com/xcbeyond/springboot/dao/UserDaoJdbcTemplateImplTest.java
+++ springboot/src/test/java/com/xcbeyond/springboot/dao/UserDaoJdbcTemplateImplTest.java
@@ -1,11 +1,19 @@
 package com.xcbeyond.springboot.dao;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.xcbeyond.springboot.SpringbootApplication;
 import com.xcbeyond.springboot.model.User;
 
-class UserDaoJdbcTemplateImplTest {
+//运行器，让测试运行于Spring测试环境
+@RunWith(SpringJUnit4ClassRunner.class)
+//SpringBoot单元测试注解，classes属性指定启动类
+@SpringBootTest(classes=SpringbootApplication.class)
+public class UserDaoJdbcTemplateImplTest {
 	@Autowired
 	private UserDao userDao;
 	
