a8cba70ae14608a9769b21b047988fab52d1d5a0
xcbeyond <xcbeyond157@163.com>
2018-08-28 15:32:27 +0800

    修改服务监控，并添加认证配置

--- springCloud/springCloudMonitor/src/main/java/com/xcbeyond/springcloud/monitor/MonitorApplication.java
+++ springCloud/springCloudMonitor/src/main/java/com/xcbeyond/springcloud/monitor/MonitorApplication.java
@@ -1,8 +1,13 @@
 package com.xcbeyond.springcloud.monitor;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 
@@ -14,33 +19,11 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableAdminServer
+@EnableAutoConfiguration
 //@EnableTurbineStream
 public class MonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
     }
-
-//    @Configuration
-//    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            // Page with login form is served as /login.html and does a POST on /login
-//            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
-//            // The UI does a POST on /logout on logout
-//            http.logout().logoutUrl("/logout");
-//            // The ui currently doesn't support csrf
-//            http.csrf().disable();
-//
-//            // Requests for the login page and the static assets are allowed
-//            http.authorizeRequests()
-//                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-//                    .permitAll();
-//            // ... and any other request needs to be authorized
-//            http.authorizeRequests().antMatchers("/**").authenticated();
-//
-//            // Enable so that the clients can authenticate via HTTP basic for registering
-//            http.httpBasic();
-//        }
-//    }
 }
---
+++ springCloud/springCloudMonitor/src/main/java/com/xcbeyond/springcloud/monitor/security/SecuritySecureConfig.java
@@ -0,0 +1,38 @@
+package com.xcbeyond.springcloud.monitor.security;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+/**
+ * 安全认证
+ * @author xcbeyond
+ * 2018年8月28日下午3:25:14
+ */
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+    private final String adminContextPath;
+
+    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf().disable();
+    }
+
+}
