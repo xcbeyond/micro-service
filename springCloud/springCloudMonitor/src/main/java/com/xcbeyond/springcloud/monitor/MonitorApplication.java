58347c8d22d8fda9c96619d1b22d365ec874fb15
xcbeyond <xcbeyond157@163.com>
2018-08-28 11:30:09 +0800

    修改服务监控，并调整依赖关系

--- springCloud/springCloudMonitor/src/main/java/com/xcbeyond/springcloud/monitor/MonitorApplication.java
+++ springCloud/springCloudMonitor/src/main/java/com/xcbeyond/springcloud/monitor/MonitorApplication.java
@@ -1,22 +1,18 @@
 package com.xcbeyond.springcloud.monitor;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 
 /**
- * 监控
+ * 服务监控
  * @author xcbeyond
  * 2018年8月27日下午4:40:43
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableAdminServer
 //@EnableTurbineStream
 public class MonitorApplication {
@@ -25,26 +21,26 @@ public class MonitorApplication {
         SpringApplication.run(MonitorApplication.class, args);
     }
 
-    @Configuration
-    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            // Page with login form is served as /login.html and does a POST on /login
-            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
-            // The UI does a POST on /logout on logout
-            http.logout().logoutUrl("/logout");
-            // The ui currently doesn't support csrf
-            http.csrf().disable();
-
-            // Requests for the login page and the static assets are allowed
-            http.authorizeRequests()
-                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-                    .permitAll();
-            // ... and any other request needs to be authorized
-            http.authorizeRequests().antMatchers("/**").authenticated();
-
-            // Enable so that the clients can authenticate via HTTP basic for registering
-            http.httpBasic();
-        }
-    }
+//    @Configuration
+//    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            // Page with login form is served as /login.html and does a POST on /login
+//            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+//            // The UI does a POST on /logout on logout
+//            http.logout().logoutUrl("/logout");
+//            // The ui currently doesn't support csrf
+//            http.csrf().disable();
+//
+//            // Requests for the login page and the static assets are allowed
+//            http.authorizeRequests()
+//                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+//                    .permitAll();
+//            // ... and any other request needs to be authorized
+//            http.authorizeRequests().antMatchers("/**").authenticated();
+//
+//            // Enable so that the clients can authenticate via HTTP basic for registering
+//            http.httpBasic();
+//        }
+//    }
 }
